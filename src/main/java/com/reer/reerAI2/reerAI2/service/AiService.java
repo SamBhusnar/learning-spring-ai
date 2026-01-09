8e0e1ea192b6fe91cabe33dfeaf2c1c11738fcca
samadhan <samadhanbhusnar21@gmail.com>
2026-01-09 11:30:04 +0530

    descriptive calling to llms rather using fluent api

--- src/main/java/com/reer/reerAI2/reerAI2/service/AiService.java
+++ src/main/java/com/reer/reerAI2/reerAI2/service/AiService.java
@@ -1,8 +1,15 @@
 package com.reer.reerAI2.reerAI2.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class AiService {
@@ -30,6 +37,21 @@ public class AiService {
     }
 
     public String enterprise(String q) {
-        return nvidiaClient.prompt(q).call().content();
+
+
+        PromptTemplate promptTemplate=new PromptTemplate(
+                """
+                tell me about  %s
+                """.formatted("java")
+        );
+        SystemPromptTemplate systemPromptTemplate = SystemPromptTemplate.builder()
+                .template("You are a helpful {name} assistant.")
+                .build();
+        Message systemMessage = systemPromptTemplate.createMessage(Map.of("name", "codding"));
+        Message userMessage = promptTemplate.createMessage();
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+        String content = nvidiaClient.prompt(prompt).call().content();
+        System.out.println(content);
+        return    content;
     }
 }
